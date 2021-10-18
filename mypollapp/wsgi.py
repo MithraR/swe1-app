580e51656235340adfb1abde7b581b55b787c300
Mithra Shanmugasundaram <ms12292@nyu.edu>
2021-10-18 10:18:04 -0400

    adding black to travis.yml

--- mypollapp/wsgi.py
+++ mypollapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypollapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mypollapp.settings")
 
 application = get_wsgi_application()
